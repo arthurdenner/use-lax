bf89136f6f7f7128c3f7aaeb31805548ebb22fe4
Arthur Denner <arthurdenner7@gmail.com>
2019-03-25 12:42:55 -0300

    chore: Configure ESLint with @typescript-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['eslint:recommended'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+};
