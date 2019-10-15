ec8d76f3e491f26b6c7763ac05527a9ff1fa88bd
Arthur Denner <arthurdenner7@gmail.com>
2019-10-15 16:50:59 +0200

    fix: update useLax hook to be compatible with latest version of lax.js
    BREAKING CHANGE: Only one hook is exported - useLax. This hook should be used in the top component.

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,24 @@ import lax from 'lax.js';
 import * as React from 'react';
 
 function useLax() {
-  lax.setup();
+  const requestRef = React.useRef<number>();
 
   React.useEffect(() => {
+    lax.setup();
+
     const updateLax = () => {
       lax.update(window.scrollY);
+      requestRef.current = window.requestAnimationFrame(updateLax);
     };
 
-    document.addEventListener('scroll', updateLax, false);
-
-    updateLax();
+    requestRef.current = window.requestAnimationFrame(updateLax);
 
     return () => {
-      document.removeEventListener('scroll', updateLax);
+      if (requestRef.current) {
+        window.cancelAnimationFrame(requestRef.current);
+      }
     };
   }, []);
 }
 
-function useLaxElement() {
-  const ref = React.useRef();
-
-  React.useEffect(() => {
-    const currentNode = ref.current;
-
-    lax.addElement(currentNode);
-
-    return () => {
-      lax.removeElement(currentNode);
-    };
-  }, []);
-
-  return ref;
-}
-
-export { useLax, useLaxElement };
+export { useLax };
