7eb2ce82e910b2e1fe30ac5903492e5d7a2377d1
Arthur Denner <arthurdenner7@gmail.com>
2020-11-23 16:52:17 +0100

    feat: add support for initial elements and addElement

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,32 @@ import * as React from 'react';
 
 console.log(lax);
 
+/**
+ * List of supported easings
+ * https://github.com/alexfoxy/lax.js#supported-easings
+ */
+type LaxEasings =
+  | 'easeInQuad'
+  | 'easeOutQuad'
+  | 'easeInOutQuad'
+  | 'easeInCubic'
+  | 'easeOutCubic'
+  | 'easeInOutCubic'
+  | 'easeInQuart'
+  | 'easeOutQuart'
+  | 'easeInOutQuart'
+  | 'easeInQuint'
+  | 'easeOutQuint'
+  | 'easeInOutQuint'
+  | 'easeOutBounce'
+  | 'easeInBounce'
+  | 'easeOutBack'
+  | 'easeInBack';
+
 export interface LaxDriverOptions {
   /**
-   * By default, each driver updates its value every animation frame, around ~60 times per second.
-   * `frameStep` can be used to reduce frequency of updates.
+   * By default each driver updates its value every animation frame, around ~60 times per second.
+   * You can use the `frameStep` to reduce frequency of the driver value updating.
    *
    * For example a value of `2` would only update ~30 times per second
    * and a value of `60` would only update about once per second.
@@ -24,6 +46,20 @@ export interface LaxDriverOptions {
   inertiaEnabled: boolean;
 }
 
+/** Map of driver names with their value and inertia  */
+export interface LaxDriverValues {
+  [driverName: string]: [string | number, number];
+}
+
+export interface LaxElementOptions {
+  style?: React.CSSProperties;
+  /**
+   * A method called every frame with the current `driverValues` and `domElement`.
+   * This could be used to toggle classes on an element or set `innerHTML`.
+   */
+  onUpdate?: (driverValues: LaxDriverValues, domElement: HTMLElement) => void;
+}
+
 export interface LaxDriver {
   /** Name of the driver. */
   name: string;
@@ -36,16 +72,79 @@ export interface LaxDriver {
   options?: LaxDriverOptions;
 }
 
+interface LaxValueOptions {
+  /**
+   * Set this option to modulus the value from the driver, for example if you want
+   * to loop the animation value as the driver value continues to increase.
+   **/
+  modValue?: number;
+  /**
+   * By default each animation updates its value every animation frame, around ~60 times per second.
+   * You can use the `frameStep` to reduce frequency of the driver value updating.
+   *
+   * For example a value of `2` would only update ~30 times per second
+   * and a value of `60` would only update about once per second.
+   *
+   * Defaults to `1`.
+   */
+  frameStep?: number;
+  /** Use to add inertia to your animations. Use in combination with the `inertiaEnabled` driver option. */
+  inertia?: number;
+  /**
+   * Use in combination with `inertia`. If set to `absolute` the inertia value
+   * will always be a positive number via the `Math.abs` operator.
+   * */
+  inertiaMode?: 'absolute' | 'normal';
+  /** Define the unit to be appended to the end of the value, e.g `px` or `deg`. */
+  cssUnit?: string;
+  /**
+   * Some CSS properties require more complex strings as values.
+   * For example, `box-shadow` has multiple values that could be modified by a lax animation.
+   */
+  cssFn?: (value: number) => string;
+  // TODO: Add description
+  easing?: LaxEasings;
+}
+
+// TODO: Find a better name for this
+type LaxValue = [
+  // TODO: Describe special values for driver value map (1st item)
+  // https://github.com/alexfoxy/lax.js#special-values
+  (number | string)[],
+  // TODO: Describe mobile breakpoints for animation value map (2nd item)
+  (number | string)[],
+  LaxValueOptions?
+];
+
+export interface LaxElement {
+  /** Selector of the element. */
+  selector: string;
+  /** Options to the element. */
+  options?: LaxElementOptions;
+  /** Map of drivers to animation values */
+  animationData?: {
+    // TODO: Try to make this autocomplete
+    [driverName: string]: {
+      // TODO: Add description and supported properties
+      // https://github.com/alexfoxy/lax.js#css-properties
+      // When property not supported, `cssFn` can be used
+      [cssProperty: string]: LaxValue;
+    };
+  };
+}
+
 export interface LaxInitOptions {
   /** Array of drivers to set when initializing lax. */
   drivers?: LaxDriver[];
+  /** Array of elements to set when initializing lax. */
+  elements?: LaxElement[];
   // breakpoints?: { [k: string]: any };
   // className?: string;
 }
 
 // let selector = 'lax';
 
-function useLax({ drivers }: LaxInitOptions = {}) {
+function useLax({ drivers, elements }: LaxInitOptions = {}) {
   // const requestRef = React.useRef<number>();
   // selector = className || selector;
 
@@ -55,6 +154,12 @@ function useLax({ drivers }: LaxInitOptions = {}) {
     if (drivers?.length) {
       drivers.forEach(({ name, getValueFn, options }) => {
         lax.addDriver(name, getValueFn, options);
+      });
+    }
+
+    if (elements?.length) {
+      elements.forEach(({ selector, animationData, options }) => {
+        lax.addElements(selector, animationData, options);
       });
     }
 
@@ -72,10 +177,13 @@ function useLax({ drivers }: LaxInitOptions = {}) {
     //     window.cancelAnimationFrame(requestRef.current);
     //   }
     // };
-  }, [drivers]);
+  }, []);
 }
 
-function useLaxElement() {
+function useLaxElement({
+  animationData,
+  options,
+}: Omit<LaxElement, 'selector'> = {}) {
   const ref = React.useRef<any>();
 
   React.useEffect(() => {
@@ -86,7 +194,7 @@ function useLaxElement() {
     // }
 
     if (currentNode) {
-      lax.addElement(currentNode);
+      lax.addElement(currentNode, animationData, options);
     }
 
     return () => {
